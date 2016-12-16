9b0760abe7347b08a43992c53adfffda96e9066c
Preston Rodriguez <prodriguez@accusoft.com>
2016-12-16 17:06:06 -0500

    Day 7

--- Day 7/IPv7.py
+++ Day 7/IPv7.py
@@ -16,17 +16,23 @@ def parse_address(addr):
     addr_list = re.findall(r'\[?[a-z]+\]?', addr)
     return addr_list
 
-def chk_address(addr_list):
-    for i in range(len(addr_list)):
-        if addr_list[i][0] == "[":
-            hnet = tls_chk(addr_list[i].strip("[]"))
+def chk_address(raw_addr):
+    addr_list = parse_address(raw_addr)
+    tls_break = False
+    tls = False
+    ssl = ssl_chk(raw_addr)
+    for addr in addr_list:
+        if addr[0] == "[":
+            hnet = tls_chk(addr.strip("[]"))
             if hnet:
-                return False
+                tls = False
+                break
         else:
-            status = tls_chk(addr_list[i])
-            if status:
-                return True
-    return False
+            if not tls_break:
+                status = tls_chk(addr)
+                if status:
+                    tls = True
+    return (tls, ssl)
 
 def tls_chk(addr_segment):
     for i in range(len(addr_segment)):
@@ -41,24 +47,62 @@ def tls_chk(addr_segment):
             return True
     return False
 
+def ssl_chk(raw_addr):
+    addr_list = parse_address(raw_addr)
+    bab_list = []
+    aba_list = []
+    for addr in addr_list:
+        addr_size = len(addr.strip("[]"))
+        if addr[0] == "[":
+            hnet = addr.strip("[]")
+            for i in range(0,addr_size):
+                if i+3 > addr_size:
+                    break
+                if hnet[i] == hnet[i+2]:
+                    bab_list.append(hnet[i:i+3])
+        else:
+            for i in range(0,addr_size):
+                if i+3 > addr_size:
+                    break
+                if addr[i] == addr[i+2]:
+                    aba_list.append(addr[i:i+3])
+    for aba in aba_list:
+        bab = "{0}{1}{0}".format(aba[1],aba[0])
+        if bab in bab_list:
+            return True
+    return False
+
+
+
 def main():
     tls = {
         'enabled' : 0,
         'disabled' : 0
         }
+    ssl = {
+        'enabled' : 0,
+        'disabled' : 0
+        }
     addr_list = load_input()
     for raw_addr in addr_list:
-        addr = parse_address(raw_addr)
-        if chk_address(addr):
+        if chk_address(raw_addr)[0]:
             tls['enabled'] += 1
         else:
             tls['disabled'] += 1
-    output = ("   ADDRESS TLS REPORT\n"
+        if chk_address(raw_addr)[1]:
+            ssl['enabled'] += 1
+        else:
+            ssl['disabled'] += 1
+    output = ("     ADDRESS REPORT\n"
               "------------------------\n"
               "TLS Enabled     : {}\n"
               "TLS Disalbed    : {}\n"
+              "SSL Enabled     : {}\n"
+              "SSL Disalbed    : {}\n"
               "Total Addresses : {}".format(tls['enabled'],
                                             tls['disabled'],
+                                            ssl['enabled'],
+                                            ssl['disabled'],
                                             len(addr_list)))
     return output
 
