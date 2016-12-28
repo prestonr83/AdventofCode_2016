71e2cd7c90142f2973a82e0d9b6240372cf4a06c
Preston Rodriguez <prestonr83@gmail.com>
2016-12-27 22:20:10 -0500

    still wip

--- Day 9/Explosive.py
+++ Day 9/Explosive.py
@@ -17,6 +17,42 @@ def load_input():
             return lines[0]
         return lines
 
+def decomp_len(line, type):
+    output = 0
+    temp = line
+    marker_dict = {}
+    calc_dict = {}
+    markers = re.findall(r'(\([0-9]+x[0-9]+\))', temp)
+    base_len = len(temp)
+    for marker in markers:
+        base_len -= len(marker)
+        mkr_pos = re.search(marker, temp)
+        mkr_select , mkr_repeat = mkr_pos.group().split('x') 
+        marker_dict[mkr_pos.start() - 1] = [int(mkr_select), int(mkr_repeat), len(marker)]
+        print "ok"
+    while True:
+        count = 0
+        for key in marker_dict.keys():
+            if key in calc_dict.keys():
+                continue
+            chk_lst = range(marker_dict[key][2]+key,key+marker_dict[key][0]+marker_dict[key][2])
+            last_marker = key
+            while True:
+                for i in chk_lst:
+                    if i in marker_dict.keys() and len(marker_dict[i]) == 3: 
+                        chk_lst = range(marker_dict[i][2]+i,i+marker_dict[i][0]+marker_dict[i][2])
+                        last_marker = i
+                        count += 1
+                        continue
+                    if len(marker_dict[i]) == 4:
+                        marker_dict[last_marker][0] += marker_dict
+                if len(marker_dict[last_marker]) == 3:    
+                    marker_dict[last_marker][0] += marker_dict[last_marker][0]+last_marker+marker_dict[last_marker][2]
+                    marker_dict[last_marker].append(True)
+                break
+        if count == 0:
+            break
+
 def decomp(line):
     output = ''
     temp = line
@@ -44,15 +80,6 @@ if __name__ == '__main__':
     expanded = decomp(data)
     length = len(expanded)
     print("Version 1 length is {}".format(length))
-    while True:
-        compressed = re.search(r'\(([0-9]+)x([0-9]+)\)', expanded)
-        round = 0
-        if compressed: 
-            round += 1
-            sys.stdout.write("Round {} = {}".format(round, len(expanded)))
-            sys.stdout.flush()
-            expanded = decomp(expanded)
-        else:
-            break
+    decomp_len(data, "test")
     v2len = len(expanded)
     print("Version 2 length is {}".format(v2len))
